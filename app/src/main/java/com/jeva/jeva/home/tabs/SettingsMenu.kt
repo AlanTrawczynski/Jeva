99a0b11642817b5301ef6df2052c93f6c6ace7e2
AlanTrawczynski <alantrawcz@gmail.com>
2021-05-30 21:23:29 +0200

    archivo faltante en último commit

---
+++ app/src/main/java/com/jeva/jeva/home/tabs/SettingsMenu.kt
@@ -0,0 +1,63 @@
+package com.jeva.jeva.home.tabs
+
+import android.app.Activity
+import android.content.Context
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.jeva.jeva.GestionarPermisos
+import com.jeva.jeva.R
+import com.jeva.jeva.home.LocaleHelper
+import com.jeva.jeva.home.PopUpActivity
+
+class SettingsMenu {
+
+    companion object {
+
+        fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+            inflater.inflate(R.menu.settings_menu,menu)
+        }
+
+
+        fun onOptionsItemSelected(item: MenuItem, view: View, activity: Activity, context: Context) {
+            val popup = PopUpActivity()
+            when(item.itemId) {
+
+                R.id.set_Spanish -> {
+                    if(!item.isChecked) {
+                        item.isChecked = true
+                        GestionarPermisos.requestRWStoragePermissions(activity)
+                        LocaleHelper.setLocale(context,"es")
+                        activity.recreate()
+                    }
+                }
+
+                R.id.set_English -> {
+                    if (!item.isChecked) {
+                        item.isChecked = true
+                        GestionarPermisos.requestRWStoragePermissions(activity)
+                        LocaleHelper.setLocale(context,"en")
+                        activity.recreate()
+                    }
+                }
+
+                R.id.sign_out -> {
+                    Firebase.auth.signOut()
+                    activity.finish()
+                }
+
+                R.id.change_email -> {
+                    popup.showPopupWindow(view, R.layout.popup_change_email, R.id.popUpChangeEmailButton, context)
+                }
+
+                R.id.change_pwd -> {
+                    popup.showPopupWindow(view, R.layout.popup_change_pwd,  R.id.popUpChangePwdButton, context)
+                }
+            }
+        }
+
+    }
+}
