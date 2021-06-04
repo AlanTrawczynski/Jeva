b04fe9c15de7303bbc2ea0614c62c6b1a27ace24
AlanTrawczynski <alantrawcz@gmail.com>
2021-06-04 19:21:20 +0200

    now changePopups have dark background

--- app/src/main/java/com/jeva/jeva/home/GenericPopUp.kt
+++ app/src/main/java/com/jeva/jeva/home/GenericPopUp.kt
@@ -1,10 +1,14 @@
 package com.jeva.jeva.home
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 import com.jeva.jeva.R
 import com.jeva.jeva.auth.AuthUtils
 import com.jeva.jeva.database.Database
@@ -18,12 +22,14 @@ class GenericPopUp {
     fun showPopupWindow(view: View, layout : Int) {
         val inflater = view.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val popupView: View = inflater.inflate(layout, null)
-        val popupWindow = PopupWindow(popupView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, true)
+        val popupWindow = PopupWindow(popupView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, true)
+        val popupBg : LinearLayout = popupView.findViewById(R.id.popupBackground)
         val closeBtn : Button = popupView.findViewById(R.id.popupBtnClose)
         val applyBtn : Button = popupView.findViewById(R.id.popupBtnApply)
 
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
 
+        popupBg.setOnClickListener { popupWindow.dismiss() }
         closeBtn.setOnClickListener { popupWindow.dismiss() }
         applyBtn.setOnClickListener {
             getApplyBtnFunction(layout, popupView, popupWindow, view.context)()
